efebb8d8e6db8524f19e73bc73c3a211707a836e
saathidavane <saathidavane@gmail.com>
2026-06-19 10:18:27 +0530

    day-8

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -1,3 +1,19 @@
 ###terraform import###
-resource "aws_s3_bucket" "s3" {
+#resource "aws_s3_bucket" "s3" {
+#    bucket = "s3-bucket-terraform-2345"
+#}
+
+###terraform loops###
+resource "aws_instance" "my_instance" {
+    for_each = tomap({
+        instance-1 = "t3.micro"
+        instance-2 = "t3.small"
+        instance-3 = "c7i-flex.large"
+    })
+    ami = "ami-0e38835daf6b8a2b9"
+    instance_type = each.value
+    tags = {
+        Name = each.key
+    }
+  
 }
---
+++ Day-8/output.tf
@@ -0,0 +1,5 @@
+###terraform loop###
+output "public_ip" {
+    value = [for instance in aws_instance.my_instance : instance.public_ip]
+  
+}
