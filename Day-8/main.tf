fae45a5386aa75d495995a66bd0e9d115d75e72a
saathidavane <saathidavane@gmail.com>
2026-06-19 10:26:50 +0530

    day-8

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -4,16 +4,24 @@
 #}
 
 ###terraform loops###
-resource "aws_instance" "my_instance" {
-    for_each = tomap({
-        instance-1 = "t3.micro"
-        instance-2 = "t3.small"
-        instance-3 = "c7i-flex.large"
-    })
-    ami = "ami-0e38835daf6b8a2b9"
-    instance_type = each.value
-    tags = {
-        Name = each.key
-    }
-  
+#resource "aws_instance" "my_instance" {
+#    for_each = tomap({
+#        instance-1 = "t3.micro"
+#        instance-2 = "t3.small"
+#        instance-3 = "c7i-flex.large"
+#    })
+#    ami = "ami-0e38835daf6b8a2b9"
+#    instance_type = each.value
+#    tags = {
+#        Name = each.key
+#    }  
+#}
+
+###terraform workspace###
+resource "random_id" "random_id" {
+    byte_length = 8
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+    bucket = "demo-bucket-${terraform.workspace}-${random_id.random_id.hex}"
 }
--- Day-8/output.tf
+++ Day-8/output.tf
@@ -1,5 +1,4 @@
 ###terraform loop###
-output "public_ip" {
-    value = [for instance in aws_instance.my_instance : instance.public_ip]
-  
-}
+#output "public_ip" {
+#    value = [for instance in aws_instance.my_instance : instance.public_ip]
+#  }
